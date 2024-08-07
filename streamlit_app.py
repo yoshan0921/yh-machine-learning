d0af9622d5c1a8cb2ef15b2e49ee72b318cbcd04
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 23:23:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,6 +82,6 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ["Adelie", "Chinstrap", "Gentoo"]
-df_prediction_proba
+prediction_proba
 
   
