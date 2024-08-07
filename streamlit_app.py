f3a6071f28d8df812eecfbe48dc71fa1fddbea84
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 22:48:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,19 +42,19 @@ with st.sidebar:
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-  # Encode X
-  encode = ["island", "sex"]
-  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-  input_row = df_penguins[:1]
+# Encode X
+encode = ["island", "sex"]
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
 
-  # Encode y
-  target_mapper = {"Adelie": 0,
-                  "Chinstrap": 1,
-                  "Gentoo": 2}
-  def target_encode(val):
-      return target_mapper[val]
-  y = y_raw.apply(target_encode)
-  y
+# Encode y
+target_mapper = {"Adelie": 0,
+                "Chinstrap": 1,
+                "Gentoo": 2}
+def target_encode(val):
+    return target_mapper[val]
+y = y_raw.apply(target_encode)
+y
     
 with st.expander("Input features"):
   st.write("**Input penguin**")
