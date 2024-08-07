6f061f2165b627f9c606cc1e4c1709cf3b801858
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 22:46:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ with st.expander("Data"):
   df
 
   st.write("**X**")
-  X = df.drop("species", axis=1)
-  X
+  X_raw = df.drop("species", axis=1)
+  X_raw
   
   st.write("**Y**")
-  y = df.species
-  y
+  y_raw = df.species
+  y_raw
 
 
 with st.expander("Data visualization"):
@@ -40,19 +40,26 @@ with st.sidebar:
           "body_mass_g": body_mass_g,
           "sex": sex}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-  # Encode
+  # Encode X
   encode = ["island", "sex"]
   df_penguins = pd.get_dummies(input_penguins, prefix=encode)
   input_row = df_penguins[:1]
 
+  # Encode y
+  target_mapper = {"Adelie": 0,
+                  "Chinstrap": 1,
+                  "Gentoo": 2}
+  def target_encode(val):
+      return target_mapper[val]
+    
 with st.expander("Input features"):
   st.write("**Input penguin**")
   input_df
   st.write("**Combined penguins data**")
   input_penguins
-  st.write("Encode input penguins")
+  st.write("**Encode input penguins**")
   input_row
 
 
