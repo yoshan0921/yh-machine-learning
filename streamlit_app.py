21cfe8c1a157f7a5edfc126da700e72ce7e7e167
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 22:35:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ with st.sidebar:
   bill_depth_mm = st.slider("Bill depth (mm)", 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider("Flipper length (mm)", 172.0, 231.0, 201.0)
   body_mass_g = st.slider("Body mass (g)", 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox("Gendar", ("Male", "Female"))
+  gender = st.selectbox("Sex", ("Male", "Female"))
 
   # Create Dataframe for the input features
   data = {"island": island,
@@ -38,7 +38,7 @@ with st.sidebar:
           "bill_depth_mm": bill_depth_mm,
           "flipper_length_mm": flipper_length_mm,
           "body_mass_g": body_mass_g,
-          "gender": gender}
+          "sex": gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 
@@ -49,7 +49,7 @@ with st.expander("Input features"):
   input_penguins
 
 # Encode
-encode = ["island", "gender"]
+encode = ["island", "sex"]
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 df_penguins[:1]
   
