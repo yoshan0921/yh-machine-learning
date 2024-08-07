afd26abbd5f44dd32c3e6fa9119c173bbfcc6b92
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 21:28:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('The purpose of this application is to experience the process of creating predictive models easily in Python and scikit-learn.')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
