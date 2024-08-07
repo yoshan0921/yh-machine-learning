1b062b546a0acfcb576ce0fb2728aac4a729cc7e
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 22:07:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ with st.sidebar:
 
   # Create Dataframe for the input features
   data = {"island": island,
-          "bill_length": bill_length_mm,
-          "bill_depth": bill_depth_mm,
+          "bill_length_mm": bill_length_mm,
+          "bill_depth_mm": bill_depth_mm,
           "flipper_length_mm": flipper_length_mm,
           "body_mass_g": body_mass_g,
           "gender": gender}
