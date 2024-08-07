d1132adc80e88b39f5ec2f036ece841f542d6c34
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-06 21:41:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,7 @@ with st.expander("Data"):
   y = df.species
   y
 
+
+with.expander("Data visualization")
+  # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
+  st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g", color="species")
